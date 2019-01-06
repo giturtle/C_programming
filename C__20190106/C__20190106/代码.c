8b52ff75d5e05e76bc621646f75c2d6885a5f50a
giturtle <851520508@qq.com>
2019-01-06 23:24:04 +0800

    C训练 数组专题

---
+++ C__20190106/C__20190106/代码.c
@@ -0,0 +1,93 @@
+#define CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	//一维数组
+
+	int a[] = { 1,2,3,4 };
+	printf("%d\n", sizeof(a));
+	printf("%d\n", sizeof(a + 0));
+	printf("%d\n", sizeof(*a));
+	printf("%d\n", sizeof(a + 1));
+	printf("%d\n", sizeof(a[1]));
+	printf("%d\n", sizeof(&a));
+	printf("%d\n", sizeof(*&a));
+	printf("%d\n", sizeof(&a + 1));
+	printf("%d\n", sizeof(&a[0]));
+	printf("%d\n", sizeof(&a[0] + 1));
+
+	//字符数组
+	char arr[] = { 'a','b','c','d','e','f' };
+	printf("%d\n", sizeof(arr));
+	printf("%d\n", sizeof(arr + 0));
+	printf("%d\n", sizeof(*arr));
+	printf("%d\n", sizeof(arr[1]));
+	printf("%d\n", sizeof(&arr));
+	printf("%d\n", sizeof(&arr + 1));
+	printf("%d\n", sizeof(&arr[0] + 1));
+
+	printf("%d\n", strlen(arr));
+	printf("%d\n", strlen(arr + 0));
+	printf("%d\n", strlen(*arr));
+	printf("%d\n", strlen(arr[1]));
+	printf("%d\n", strlen(&arr));
+	printf("%d\n", strlen(&arr + 1));
+	printf("%d\n", strlen(&arr[0] + 1));
+
+	char arr[] = "abcdef";
+	printf("%d\n", sizeof(arr));
+	printf("%d\n", sizeof(arr + 0));
+	printf("%d\n", sizeof(*arr));
+	printf("%d\n", sizeof(arr[1]));
+	printf("%d\n", sizeof(&arr));
+	printf("%d\n", sizeof(&arr + 1));
+	printf("%d\n", sizeof(&arr[0] + 1));
+
+	printf("%d\n", strlen(arr));
+	printf("%d\n", strlen(arr + 0));
+	printf("%d\n", strlen(*arr));
+	printf("%d\n", strlen(arr[1]));
+	printf("%d\n", strlen(&arr));
+	printf("%d\n", strlen(&arr + 1));
+	printf("%d\n", strlen(&arr[0] + 1));
+
+	char *p = "abcdef";
+	printf("%d\n", sizeof(p));
+	printf("%d\n", sizeof(p + 1));
+	printf("%d\n", sizeof(*p));
+	printf("%d\n", sizeof(p[0]));
+	printf("%d\n", sizeof(&p));
+	printf("%d\n", sizeof(&p + 1));
+	printf("%d\n", sizeof(&p[0] + 1));
+
+	printf("%d\n", strlen(p));
+	printf("%d\n", strlen(p + 1));
+	printf("%d\n", strlen(*p));
+	printf("%d\n", strlen(p[0]));
+	printf("%d\n", strlen(&p));
+	printf("%d\n", strlen(&p + 1));
+	printf("%d\n", strlen(&p[0] + 1));
+
+	//二维数组
+	int a[3][4] = { 0 };
+	printf("%d\n", sizeof(a));
+	printf("%d\n", sizeof(a[0][0]));
+	printf("%d\n", sizeof(a[0]));
+	printf("%d\n", sizeof(a[0] + 1));
+	printf("%d\n", sizeof(*(a[0] + 1)));
+	printf("%d\n", sizeof(a + 1));
+	printf("%d\n", sizeof(*(a + 1)));
+	printf("%d\n", sizeof(&a[0] + 1));
+	printf("%d\n", sizeof(*(&a[0] + 1)));
+	printf("%d\n", sizeof(*a));
+	printf("%d\n", sizeof(a[3]));
+
+	//1. sizeof(数组名)，这里的数组名表示整个数组，计算的是整个数组的大小。
+	//2. &数组名，这里的数组名表示整个数组，取出的是整个数组的地址。
+	//3. 除此之外所有的数组名都表示首元素的地址。
+
+	system("pause");
+	return 0;
+}
