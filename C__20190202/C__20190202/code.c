83330022eac8fd25cb22ea882dd57e490aedb50f
giturtle <851520508@qq.com>
2019-02-02 23:21:42 +0800

    字符串库函数模拟实现（一）

--- C__20190202/C__20190202/code.c
+++ C__20190202/C__20190202/code.c
@@ -1,89 +1,117 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 
-//
-////指针话题复习
-//int main() {
-//
-//	//一维数组
-//	int a[] = { 1,2,3,4 };
-//	printf("%d\n", sizeof(a));
-//	printf("%d\n", sizeof(a + 0));
-//	printf("%d\n", sizeof(*a));
-//	printf("%d\n", sizeof(a + 1));
-//	printf("%d\n", sizeof(a[1]));
-//	printf("%d\n", sizeof(&a));
-//	printf("%d\n", sizeof(*&a));
-//	printf("%d\n", sizeof(&a + 1));
-//	printf("%d\n", sizeof(&a[0]));
-//	printf("%d\n", sizeof(&a[0] + 1));
-//
-//	//字符数组
-//	char arr[] = { 'a','b','c','d','e','f' };
-//	printf("%d\n", sizeof(arr));
-//	printf("%d\n", sizeof(arr + 0));
-//	printf("%d\n", sizeof(*arr));
-//	printf("%d\n", sizeof(arr[1]));
-//	printf("%d\n", sizeof(&arr));
-//	printf("%d\n", sizeof(&arr + 1));
-//	printf("%d\n", sizeof(&arr[0] + 1));
-//
-//	printf("%d\n", strlen(arr));
-//	printf("%d\n", strlen(arr + 0));
-//	printf("%d\n", strlen(*arr));
-//	printf("%d\n", strlen(arr[1]));
-//	printf("%d\n", strlen(&arr));
-//	printf("%d\n", strlen(&arr + 1));
-//	printf("%d\n", strlen(&arr[0] + 1));
-//
-//	char arr[] = "abcdef";
-//	printf("%d\n", sizeof(arr));
-//	printf("%d\n", sizeof(arr + 0));
-//	printf("%d\n", sizeof(*arr));
-//	printf("%d\n", sizeof(arr[1]));
-//	printf("%d\n", sizeof(&arr));
-//	printf("%d\n", sizeof(&arr + 1));
-//	printf("%d\n", sizeof(&arr[0] + 1));
-//
-//	printf("%d\n", strlen(arr));
-//	printf("%d\n", strlen(arr + 0));
-//	printf("%d\n", strlen(*arr));
-//	printf("%d\n", strlen(arr[1]));
-//	printf("%d\n", strlen(&arr));
-//	printf("%d\n", strlen(&arr + 1));
-//	printf("%d\n", strlen(&arr[0] + 1));
-//
-//	char *p = "abcdef";
-//	printf("%d\n", sizeof(p));
-//	printf("%d\n", sizeof(p + 1));
-//	printf("%d\n", sizeof(*p));
-//	printf("%d\n", sizeof(p[0]));
-//	printf("%d\n", sizeof(&p));
-//	printf("%d\n", sizeof(&p + 1));
-//	printf("%d\n", sizeof(&p[0] + 1));
-//
-//	printf("%d\n", sizeof(p));
-//	printf("%d\n", sizeof(p + 1));
-//	printf("%d\n", sizeof(*p));
-//	printf("%d\n", sizeof(p[0]));
-//	printf("%d\n", sizeof(&p));
-//	printf("%d\n", sizeof(&p + 1));
-//	printf("%d\n", sizeof(&p[0] + 1));
-//	
-//	//二维数组
-//	int a[3][4] = { 0 };
-//	printf("%d\n", sizeof(a));
-//	printf("%d\n", sizeof(a[0][0]));
-//	printf("%d\n", sizeof(a[0]));
-//	printf("%d\n", sizeof(a[0] + 1));
-//	printf("%d\n", sizeof(*(a[0] + 1)));
-//	printf("%d\n", sizeof(a + 1));
-//	printf("%d\n", sizeof(*(a + 1)));
-//	printf("%d\n", sizeof(&a[0] + 1));
-//	printf("%d\n", sizeof(*(&a[0] + 1)));
-//	printf("%d\n", sizeof(*a));
-//	printf("%d\n", sizeof(a[3]));
-//
-//
-//
-//	system("pause");
-//	return 0;
-//}
+//strlen
+
+size_t Strlen(const char *str) {
+	assert(str != NULL);
+	size_t val = 0;
+	while (*str) {
+		val++;
+		str++;
+	}
+	return val;
+}
+int main() {
+	char *str = "giturtle";
+	printf("%lu\n",Strlen(str));
+	system("pause");
+	return 0;
+}
+
+
+
+//strcpy
+
+char* Strcpy(char *dest, const char *src) {
+	assert(dest != NULL);
+	assert(src != NULL);
+	int i = 0;	
+	for (; src[i] != '\0'; i++) {
+		dest[i] = src[i];
+	}
+	/*while (src[i] != '\0') {
+		dest[i] = src[i];	
+		i++;
+	}*/
+	dest[i] = '\0';
+	return dest;
+}
+
+int main() {
+	char dest[1024] = "niubi";
+	const char src[20] = "haha";
+	Strcpy(dest, src);
+	printf("%s\n", dest);
+	system("pause");
+	return 0;
+}
+
+
+
+int Strcmp(const char * str1, const char * str2) {
+	assert(str1 != NULL);
+	assert(str2 != NULL);
+	while (*str1 != '\0' && *str2 != '\0') {
+		if (*str1 < *str2) {
+			break;
+		}
+		else if (*str1 > *str2) {
+			break;
+		}
+		else {
+			++str1;
+			++str2;
+		}
+	}
+	/*if (*str1 == '\0' && *str2 != '\0') {
+		return -1;
+	}
+	if (*str1 != '\0' && *str2 == '\0') {
+		return 1;
+	}
+	if (*str1 == '\0' && *str2 == '\0') {
+		return 0;
+	}*/
+	if (*str1 < *str2) {
+		return -1;
+	}
+	else if (*str1 > *str2) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+int main() {
+	char *str1 = "woshizhu";
+	char *str2 = "woshizhu";
+	printf("%d\n",Strcmp(str1, str2));
+	system("pause");
+	return 0;
+}
+
+char *Strcat(char *dest, const char *src) {
+	assert(dest != NULL);
+	assert(src != NULL);
+	int i = 0;
+	for (; dest[i] != '\0'; ++i);
+	for (int j = 0; src[j] != '\0'; ++j, ++i) {
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
+	return dest;
+}
+int main() {
+	char dest[30] = "woshi";
+	const char src[10] = "cowboy";
+	Strcat(dest, src);
+	printf("%s\n",dest);
+	system("pause");
+	return 0;
+}
+
+
