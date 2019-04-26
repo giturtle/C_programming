704f3f3017ea7aa3226adde530935245e9591a79
giturtle <44741844+giturtle@users.noreply.github.com>
2019-04-26 23:36:58 +0800

    reverse String

---
+++ reverse_String.c
@@ -0,0 +1,41 @@
+
+//给定一个字符串，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
+
+//示例 1:
+//输入: "Let's take LeetCode contest"
+//输出: "s'teL ekat edoCteeL tsetnoc" 
+
+
+//解法一
+void reverse(char *s,int begin,int index){
+    while(begin < index){
+        char t = s[begin];
+        s[begin] = s[index];
+        s[index] = t;
+        begin++;
+        index--;
+    }
+}
+char* reverseWords(char* s) {
+    if(s == NULL){
+        return NULL;
+    }
+    int begin = 0;
+    int i = 0;
+    int len = strlen(s);
+    for(;i < len;i++){
+        if(s[i] != ' ' && i != len - 1){
+            continue;
+        }
+        if(i != len - 1){
+            reverse(s,begin,i-1);
+            begin = i + 1;
+        }
+        else{
+            reverse(s,begin,i);
+        }
+    }
+    return s;
+  
+   
+}
